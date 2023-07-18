d7d551a97ecb8e919ee100898eefc2825cbe42a5
Parv Shah <parvthecoder@gmail.com>
2023-07-18 21:01:29 +0530

    first commit

--- Total Prize Money/Codechef.java
+++ Total Prize Money/Codechef.java
@@ -1,21 +1,18 @@
 import java.util.*;
-import java.lang.*;
-import java.io.*;
 
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		Scanner sc= new Scanner(System.in);
-        int t= sc.nextInt();
-        while (t-->0) {
+class Codechef {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
 
-            int x= sc.nextInt();
-            int y=sc.nextInt();
+            int x = sc.nextInt();
+            int y = sc.nextInt();
 
-            int total= x*10 + y*90;
+            int total = x * 10 + y * 90;
             System.out.println(total);
-            
+
         }
-	}
+        sc.close();
+    }
 }
