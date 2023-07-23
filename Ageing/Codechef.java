e797a4090d15c929ee5871fbaa473df4b0f4574b
Parv Shah <parvthecoder@gmail.com>
2023-07-23 23:32:23 +0530

    initial commit

---
+++ Ageing/Codechef.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef {
+    public static void main(String[] args) throws java.lang.Exception {
+        int chef = 20;
+        int chefina = 10;
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int age = sc.nextInt();
+            int difference = age - chef;
+            int result = chefina + difference;
+            System.out.println(result);
+        }
+    }
+}
