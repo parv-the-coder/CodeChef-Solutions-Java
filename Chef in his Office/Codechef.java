8336b4721c035c1dd38e2fc572e2cca5ddd51038
Parv Shah <parvthecoder@gmail.com>
2024-02-09 13:08:00 +0530

    initial commit

---
+++ Chef in his Office/Codechef.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef {
+    public static void main(String[] args) throws java.lang.Exception {
+        // your code goes here
+
+    }
+}
