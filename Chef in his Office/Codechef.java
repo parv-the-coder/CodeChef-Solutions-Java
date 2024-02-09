7461874a9e21e4737583d2f03f981d470b1ab2a1
Parv Shah <parvthecoder@gmail.com>
2024-02-09 13:10:18 +0530

    initial commit

--- Chef in his Office/Codechef.java
+++ Chef in his Office/Codechef.java
@@ -4,7 +4,12 @@ import java.io.*;
 
 class Codechef {
     public static void main(String[] args) throws java.lang.Exception {
-        // your code goes here
-
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            System.out.println(x * 4 + y);
+        }
     }
 }
