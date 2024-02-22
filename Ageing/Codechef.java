c1c82a6c7833ffeb9abcf791b039030d03bec266
Parv Shah <parvthecoder@gmail.com>
2024-02-22 15:23:33 +0530

    commit

--- Ageing/Codechef.java
+++ Ageing/Codechef.java
@@ -1,6 +1,5 @@
 import java.util.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
 class Codechef {
     public static void main(String[] args) throws java.lang.Exception {
         int chef = 20;
